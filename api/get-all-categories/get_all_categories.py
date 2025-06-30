132489abbfcd0135c6b52414b64ac0e3ad3189b6
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-30 10:16:00 +0100

    Get all categories lambda code created and requirments.txt.

---
+++ api/get-all-categories/get_all_categories.py
@@ -0,0 +1,35 @@
+import datetime
+import os
+import json
+import psycopg2
+
+def json_default(obj):
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    return str(obj)
+
+def lambda_handler(event, context):
+    try:
+        with psycopg2.connect(
+            dbname=os.environ['DB_NAME'],
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASS'],
+            host=os.environ['DB_HOST'],
+            port=os.environ['DB_PORT']
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT * FROM categories;")
+                rows = cursor.fetchall()
+                columns = [desc[0] for desc in cursor.description]
+                categories = [dict(zip(columns, row)) for row in rows]
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(categories, default=json_default)
+        }
+        
+    except psycopg2.Error as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Database error'})
+        }
