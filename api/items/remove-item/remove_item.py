b8e4b12c652a2a7d3aa9597bf1b43a991de15d54
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-07-01 09:50:14 +0100

    FIX: SQL amended to set deleted to true instead of false.

--- api/items/remove-item/remove_item.py
+++ api/items/remove-item/remove_item.py
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
                 cursor.execute(
                     """
                     UPDATE items
-                    SET deleted = false
+                    SET deleted = TRUE
                     WHERE id = %s
                     RETURNING id;
                     """,
