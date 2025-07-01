0b9c217b268e402bfc0eb949f5986ff89900f610
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-07-01 15:21:03 +0100

    Lambda layer created for db connection.

---
+++ api/lambda-layers/db-connection/python/db_connection.py
@@ -0,0 +1,11 @@
+import os
+import psycopg2
+
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASS'],
+        host=os.environ['DB_HOST'],
+        port=os.environ['DB_PORT']
+    )
