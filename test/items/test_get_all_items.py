223dbffb6c6d992ca868a77ac38e5fbc6944810b
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-07-03 09:43:46 +0100

    Testing for get all items method added to test file.

--- test/items/test_get_all_items.py
+++ test/items/test_get_all_items.py
@@ -2,20 +2,21 @@ import unittest
 import psycopg2
 from unittest.mock import MagicMock, patch
 import json
-from api.categories.get_all_categories.get_all_categories import lambda_handler, json_default
+from api.items.get_all_items.get_all_items import lambda_handler
 import datetime
 
-class TestGetAllCategories(unittest.TestCase):
+class TestGetAllItems(unittest.TestCase):
     def setUp(self):
         self.mock_description = [
-            ("id",), ("name",), ("created_at",)
+            ("id",), ("name",), ("price",), ("description",), ("created_at",)
         ]
 
-    @patch("api.categories.get_all_categories.get_all_categories.get_db_connection")
-    def test_lambda_handler_returns_expected_category(self, mock_get_db_connection):
+
+    @patch("api.items.get_all_items.get_all_items.get_db_connection")
+    def test_lambda_handler_returns_expected_items(self, mock_get_db_connection):
         mock_cursor = MagicMock()
         mock_cursor.fetchall.return_value = [
-            (1, "Test Category", datetime.datetime(2025, 7, 2, 12, 0, 0))
+            (1, "Test Item", 1.99, "Test Item Description", datetime.datetime(2025, 7, 2, 12, 0, 0))
         ]
         mock_cursor.description = self.mock_description
 
@@ -23,31 +24,31 @@ class TestGetAllCategories(unittest.TestCase):
         mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
         mock_get_db_connection.return_value.__enter__.return_value = mock_conn
 
-        expectedResponseBody = [{'id': 1, 'name': 'Test Category', 'created_at': '2025-07-02T12:00:00'}]
+        expectedResponseBody = [{'id': 1, 'name': 'Test Item', 'price': '1.99', 'created_at': '2025-07-02T12:00:00'}]
 
         response = lambda_handler({},None)
 
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(json.loads(response['body']), expectedResponseBody)
 
-    @patch("api.categories.get_all_categories.get_all_categories.get_db_connection")
-    def test_lambda_handler_returns_expected_categories(self, mock_get_db_connection):
+    @patch("api.items.get_all_items.get_all_items.get_db_connection")
+    def test_lambda_handler_returns_expected_items(self, mock_get_db_connection):
         mock_cursor = MagicMock()
-        mock_cursor.fetchall.return_value = [(1, "Test Category", datetime.datetime(2025, 7, 2, 12, 0, 0)), (2, "Test Category 2", datetime.datetime(2025, 7, 2, 12, 0, 0))]
+        mock_cursor.fetchall.return_value = [(1, "Test Item", 1.99, "Test Item Description", datetime.datetime(2025, 7, 2, 12, 0, 0)),(2, "Test Item 2", 2.99, "Test Item 2 Description", datetime.datetime(2025, 7, 2, 12, 0, 0))]
         mock_cursor.description = self.mock_description
 
         mock_conn = MagicMock()
         mock_conn.cursor.return_value.__enter__.return_value = mock_cursor
         mock_get_db_connection.return_value.__enter__.return_value = mock_conn
 
-        expectedResponseBody = [{'id': 1, 'name': 'Test Category', 'created_at': '2025-07-02T12:00:00'}, {'id': 2, 'name': 'Test Category 2', 'created_at': '2025-07-02T12:00:00'}]
+        expectedResponseBody = [{'id': '1', 'name': 'Test Item', 'price': '1.99', 'description': 'Test Item Description', 'created_at': '2025-07-02 12:00:00'}, {'id': '2', 'name': 'Test Item 2', 'description': 'Test Item 2 Description', 'price': '2.99', 'created_at': '2025-07-02 12:00:00'}]
 
         response = lambda_handler({},None)
 
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(json.loads(response['body']), expectedResponseBody)
 
-    @patch("api.categories.get_all_categories.get_all_categories.get_db_connection")
+    @patch("api.items.get_all_items.get_all_items.get_db_connection")
     def test_lambda_handler_returns_empty_list_if_empty_db_return(self, mock_get_db_connection):
         mock_cursor = MagicMock()
         mock_cursor.fetchall.return_value = []
@@ -64,7 +65,7 @@ class TestGetAllCategories(unittest.TestCase):
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(json.loads(response['body']), expectedResponseBody)
 
-    @patch("api.categories.get_all_categories.get_all_categories.get_db_connection")
+    @patch("api.items.get_all_items.get_all_items.get_db_connection")
     def test_lambda_handler_throws_database_error(self, mock_get_db_connection):
         mock_get_db_connection.side_effect = psycopg2.Error('DB Error')
 
@@ -72,15 +73,3 @@ class TestGetAllCategories(unittest.TestCase):
 
         self.assertEqual(response['statusCode'], 500)
         self.assertIn('Database error', response['body'])
-
-    def test_default_json_handles_datetime(self):
-        dateObj = datetime.datetime(2025, 7, 2)
-        self.assertEqual(dateObj.isoformat(), json_default(dateObj))
-
-    def test_default_json_handles_string(self):
-        stringObj = "String Object"
-        self.assertEqual("String Object", json_default(stringObj))
-
-    def test_default_json_handles_float(self):
-        floatObj = 1.99
-        self.assertEqual("1.99", json_default(floatObj))
