b1db8cf4146d7b58b81cd80902fd7c2bb1fa230b
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-07-14 11:47:34 +0100

    items-categories endpoints changed to use try/except wrapper.

--- api/item_categories/get_items_by_category/get_items_by_category.py
+++ api/item_categories/get_items_by_category/get_items_by_category.py
@@ -4,7 +4,7 @@ from utils.logger import logger
 from utils.db_connection import get_db_connection
 from utils.validation import extract_id_path_param, get_active_row_from_table
 from utils.json_default import json_default
-from utils.custom_exceptions import ValidationError, ActiveResourceNotFoundError
+from utils.lambda_exception_handler_wrapper import lambda_exception_handler_wrapper
 
 def fetch_items_by_category_from_db(category_id):
     try:
@@ -35,40 +35,14 @@ def fetch_items_by_category_from_db(category_id):
     except Exception as e:
         logger.error(f"Unexpected error while fetching all items in a category: {e}")
         raise
-    
+
+@lambda_exception_handler_wrapper  
 def lambda_handler(event, context):
-    try:
-        category_id = extract_id_path_param(event)
+    category_id = extract_id_path_param(event)
 
-        items = fetch_items_by_category_from_db(category_id)
+    items = fetch_items_by_category_from_db(category_id)
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps(items, default=json_default)
-        }
-    
-    except ValidationError as e:
-        logger.warning(f'Validation error: {e.message}')
-        return {
-            'statusCode': e.status_code,
-            'body': json.dumps({'error': e.message})
-        }
-    except ActiveResourceNotFoundError as e:
-        logger.warning(f'Resource not found: {e.message}')
-        return {
-            'statusCode': e.status_code,
-            'body': json.dumps({'error': e.message})
-        }
-    except psycopg2.Error as e:
-        logger.error(f'Database error: {e}', exc_info=True)
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Database error'})
-        }
-    except Exception as e:
-        logger.error(f'Unhandled exception: {e}', exc_info=True)
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Internal server error'})
-        }    
-
+    return {
+        'statusCode': 200,
+        'body': json.dumps(items, default=json_default)
+    }
