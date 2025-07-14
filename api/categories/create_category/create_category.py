031aa7fbd3460900e791f93f43a82dbd5e286a56
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-07-14 11:36:36 +0100

    Additional file added to utils lambda layer. Wrapper for try/except of lambda_handlers. Implemented on post category and functioning.

--- api/categories/create_category/create_category.py
+++ api/categories/create_category/create_category.py
@@ -5,6 +5,7 @@ from utils.validation import validate_category_event_body
 from utils.json_default import json_default
 from utils.custom_exceptions import ValidationError, DatabaseInsertError
 from utils.logger import logger
+from utils.lambda_exception_handler_wrapper import lambda_exception_handler_wrapper
 
 def post_category_to_db(name):
     try:
@@ -36,39 +37,14 @@ def post_category_to_db(name):
     except Exception as e:
         logger.error(f"Unexpected error while creating category: {e}")
         raise
-    
-def lambda_handler(event, context):
-    try:
-        category_name = validate_category_event_body(event)
-        create_category_response = post_category_to_db(category_name)
-        
-        logger.info(f'Create category successful: {create_category_response}')
-        return {
-            'statusCode': 201,
-            'body': json.dumps(create_category_response, default=json_default)
-        }
 
-    except ValidationError as e:
-        logger.warning(f'Validation error: {e.message}')
-        return {
-            'statusCode': e.status_code,
-            'body': json.dumps({'error': e.message})
-        }
-    except DatabaseInsertError as e:
-        logger.warning(f'Database insert error: {e.message}')
-        return {
-            'statusCode': e.status_code,
-            'body': json.dumps({'error': e.message})
-        }
-    except psycopg2.Error as e:
-        logger.error(f'Database error: {e}')
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Database error'})
-        }
-    except Exception as e:
-        logger.error(f'Unhandled exception: {e}', exc_info=True)
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Internal server error'})
-        }
+@lambda_exception_handler_wrapper    
+def lambda_handler(event, context):
+    category_name = validate_category_event_body(event)
+    create_category_response = post_category_to_db(category_name)
+    
+    logger.info(f'Create category successful: {create_category_response}')
+    return {
+        'statusCode': 201,
+        'body': json.dumps(create_category_response, default=json_default)
+    }
