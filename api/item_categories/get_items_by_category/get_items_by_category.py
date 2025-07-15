8d73ab095ce946eba99ec2cdf73ad0451def26a2
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-07-15 16:07:30 +0100

    Data parsing from row to dict removed and replaced with using real dict cursor for item categories.

--- api/item_categories/get_items_by_category/get_items_by_category.py
+++ api/item_categories/get_items_by_category/get_items_by_category.py
@@ -1,5 +1,5 @@
 import json
-import psycopg2
+import psycopg2.extras
 from utils.logger import logger
 from utils.db_connection import get_db_connection
 from utils.validation import extract_id_path_param, get_active_row_from_table
@@ -9,7 +9,7 @@ from utils.lambda_exception_handler_wrapper import lambda_exception_handler_wrap
 def fetch_items_by_category_from_db(category_id):
     try:
         with get_db_connection() as conn:
-            with conn.cursor() as cursor:
+            with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
                 get_active_row_from_table(cursor, table_name='categories', id=category_id)
 
                 cursor.execute(
@@ -23,11 +23,9 @@ def fetch_items_by_category_from_db(category_id):
                     (category_id,))
                 
                 rows = cursor.fetchall()
-                columns = [desc[0] for desc in cursor.description]
-                items = [dict(zip(columns, row)) for row in rows]
 
-                logger.info(f"Successfully fetched {len(items)} items for category {category_id}")
-                return items
+                logger.info(f"Successfully fetched {len(rows)} items for category {category_id}")
+                return rows
             
     except psycopg2.Error as e:
         logger.error(f"Database error while fetching all items in a category: {e}")
